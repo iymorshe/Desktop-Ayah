585d6281e65b9f94589aaf2aed960654329a1cd6
iymorshe <92063753+iymorshe@users.noreply.github.com>
2024-05-03 23:12:49 -0700

    x

---
+++ Menu.swift
@@ -0,0 +1,28 @@
+//
+//  Menu.swift
+//  Desktop Quran
+//
+//  Created by Iman Morshed on 5/3/24.
+//
+
+import SwiftUI
+
+
+struct Menu: View {
+    var body: some View {
+        Text("lol")
+    }
+
+    func showFonts() {}
+    func toggleBold() {}
+    func toggleItalic() {}
+    func toggleUnderline() {}
+    func makeBigger() {}
+    func makeSmaller() {}
+    func adjustKern() {}
+    func toggleLigatures() {}
+    func adjustBaseline() {}
+    func showColors() {}
+    func copyStyle() {}
+    func pasteStyle() {}
+}
